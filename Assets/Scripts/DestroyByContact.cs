eefdda155e4c2f579b99a11bec427baaaaa4c8b3
Angel Newton <xanewton@gmail.com>
2018-02-16 13:38:06 -0500

    Add explosions to Asteroid

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+
     void OnTriggerEnter(Collider other)
     {
         // Debug.Log(other.name);
@@ -11,6 +15,13 @@ public class DestroyByContact : MonoBehaviour {
         {
             return;
         }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            //gameController.GameOver();
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
